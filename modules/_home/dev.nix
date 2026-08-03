6c2a69e4a8d3470d5dab6b1a8a65e6e829542bb7
rushevich <george@rushevich.com>
2026-08-03 19:36:19 -0400

    screw js

--- modules/_home/dev.nix
+++ modules/_home/dev.nix
@@ -1,33 +1,34 @@
-home.packages = with pkgs; [
-  # C/C++ toolchain
-  gcc16
-  clang
-  clang-tools        # clangd
-  cmake
-  ninja
-  gnumake
-  gdb
-  lldb
-  pkg-config
+{ pkgs , ... }: {
+	home.packages = with pkgs; [
+# C/C++ toolchain
+		gcc16
+			clang
+			clang-tools        # clangd
+			cmake
+			ninja
+			gnumake
+			gdb
+			lldb
+			pkg-config
 
-  # scripting
-  python3
-  nodejs
+# scripting
+			python3
 
-  # language servers (for editor LSP everywhere)
-  lua-language-server
-  pyright
+# language servers (for editor LSP everywhere)
+			lua-language-server
+			pyright
 
-  # core dev utilities
-  git
-  ripgrep
-  fd
-  jq
-  tree
-  curl
-  wget
+# core dev utilities
+			git
+			ripgrep
+			fd
+			jq
+			tree
+			curl
+			wget
 
-  # nix tooling
-  nixfmt-rfc-style
-  nil                # nix language server
-];
+# nix tooling
+			nixfmt-rfc-style
+			nil                # nix language server
+			];
+		       }
