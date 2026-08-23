ddb985eae31b9002b91c7ec51a283ddf967cdda8
rushevich <george@rushevich.com>
2026-08-23 12:55:46 -0400

    zsh prompt stuff

--- modules/_home/zsh.nix
+++ modules/_home/zsh.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -9,5 +9,16 @@
     history.ignoreAllDups = true;
     history.path = "$HOME/.zsh_history";
     history.ignorePatterns = ["rm *" "pkill *" "cp *"];
+    # host
+    initContent = lib.mkAfter ''
+    setopt PROMPT_SUBST
+    
+    autoload -Uz vcs_info
+    precmd() { vcs_info }
+    zstyle ':vcs_info:git:*' formats '%b '
+             
+    PROMPT='%F{}%f%F{#4682b4}%~%f%F{#eee685} ''${vcs_info_msg_0_}%f $ '
+    RPROMPT='%F{#131313}%*%f'
+    '';
   };
 }
