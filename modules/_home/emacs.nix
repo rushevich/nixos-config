dfd773a35858188a27ebf364f08a7022da0259fb
George Rushevich <george@rushevich.com>
2026-08-29 22:59:54 -0400

    update emacs to version 31

--- modules/_home/emacs.nix
+++ modules/_home/emacs.nix
@@ -1,21 +1,27 @@
-{ config, pkgs, lib, ... }: {
-	home.packages = with pkgs; [
-		emacs-pgtk
-		ripgrep
-		fd
-		git
-	];
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  home.packages = with pkgs; [
+    emacs31-pgtk
+    ripgrep
+    fd
+    git
+  ];
 
-	home.activation.cloneDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-		if [ ! -d "${config.home.homeDirectory}/dotfiles" ]; then
-			${pkgs.git}/bin/git clone https://github.com/rushevich/general-dotfiles "${config.home.homeDirectory}/dotfiles"
-				fi
-				'';
-	xdg.configFile."emacs".source =
-		config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/emacs/.emacs.d";
+  home.activation.cloneDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    		if [ ! -d "${config.home.homeDirectory}/dotfiles" ]; then
+    			${pkgs.git}/bin/git clone https://github.com/rushevich/general-dotfiles "${config.home.homeDirectory}/dotfiles"
+    				fi
+    				'';
+  xdg.configFile."emacs".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/emacs/.emacs.d";
 
   services.emacs = {
     enable = true;
-    package = pkgs.emacs-pgtk;
+    package = pkgs.emacs31-pgtk;
   };
 }
