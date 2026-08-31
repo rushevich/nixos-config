f012afaa4bc97ca2284cb386ee0b3c5186013cde
George Rushevich <george@rushevich.com>
2026-08-31 10:51:47 -0400

    change to slang-server

--- modules/_home/dev.nix
+++ modules/_home/dev.nix
@@ -1,34 +1,34 @@
-{ pkgs , ... }: {
-	home.packages = with pkgs; [
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
     # C/C++ toolchain
-		gcc16
-		clang-tools        # clangd
-		cmake
-		ninja
-		gnumake
-		gdb
-		lldb
-		pkg-config
+    gcc16
+    clang-tools # clangd
+    cmake
+    ninja
+    gnumake
+    gdb
+    lldb
+    pkg-config
 
     # scripting
-		python3
+    python3
 
     # language servers (for editor LSP everywhere)
-		lua-language-server
-		pyright
-                svls
-                
+    lua-language-server
+    pyright
+    slang-server
+
     # core dev utilities
-		git
-		ripgrep
-		fd
-		jq
-		tree
-		curl
-		wget
+    git
+    ripgrep
+    fd
+    jq
+    tree
+    curl
+    wget
 
     # nix tooling
-		nixfmt
-		nil                # nix language server
-	];
+    nixfmt
+    nil # nix language server
+  ];
 }
